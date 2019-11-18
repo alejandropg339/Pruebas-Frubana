3c02b76eca377094836dc496cd8659e7fa6ffad4
alejandropg339 <57865025+alejandropg339@users.noreply.github.com>
2019-11-18 00:43:18 -0500

    Update Principal.java

--- src/co/emp/fruabana/mediana/Principal.java
+++ src/co/emp/fruabana/mediana/Principal.java
@@ -2,11 +2,13 @@ package co.emp.fruabana.mediana;
 
 /**
  *
- * @author USUARIO
+ * @author Alejandro Padilla
  */
 public class Principal {
     public static void main(String[] args) {
+        //Realizamos la instancia de la clase que contiene el metodo
         Mediana m =new Mediana();
+       //LLamamos al metodo
         m.calcularMediana();
     }
 }
